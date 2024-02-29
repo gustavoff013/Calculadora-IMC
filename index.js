177d313fdec57771983940011c47987f59dcbe57
Gustavo Franco França <gustavo.franco@academico.ifpb.edu.br>
2024-02-29 20:05:17 -0300

    Finalizando o estilo da calculadora

---
+++ index.js
@@ -0,0 +1,15 @@
+/*const calcular = document.getElementById('calcular');
+
+function imc(){
+    const nome = document.getElementById('input-name').value;
+    const altura = document.getElementById('input-altura').value;
+    const peso = document.getElementById('input-peso').value;
+    const resultado = document.getElementById('resultado');
+    
+    if(nome === '' && altura === '' && peso === ''){
+        resultado.textContent = 'Preencha os campos'
+    }
+}
+
+calcular.addEventListener('click', imc); // o método addEventListener recebe como primeiro parâmetro o nome do evento, e como segundo parâmetro a função a ser executada.
+*/
